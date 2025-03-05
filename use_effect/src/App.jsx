06329664c03ae6ddaf5aa0971c8dc925d45cc676
italiya taksh <italiyataksh088@gmail.com>
2025-03-05 10:58:03 +0530

    3-5-25

--- use_effect/src/App.jsx
+++ use_effect/src/App.jsx
@@ -9,12 +9,18 @@ function App() {
     setcounst(count);
   }
 
+  function dec(){
+    if(count == 0){return false}
+    count --;
+    setcounst(count);
+  }
+
   return (
     <>
       <center>
           <h1>Counter = 0{count}</h1>
           <button className="btn btn-success" onClick={update}>➕</button>
-          <button className="btn btn-danger ms-3" onClick={update}>➖</button>
+          <button className="btn btn-danger ms-3" onClick={dec}>➖</button>
       </center>
     </>
   )
